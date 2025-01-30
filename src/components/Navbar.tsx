ae31c9b4967132db36036b78b02403d7e34bf57c
kxhxx <118926135+kxhxx@users.noreply.github.com>
2025-01-30 22:26:56 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
             <input
               type="search"
-              placeholder="Search"
+              placeholder="Search User"
               className="w-full pl-10 pr-4 py-2 bg-gray-100 rounded-full focus:outline-none focus:ring-2 focus:ring-depop-red"
             />
           </div>
